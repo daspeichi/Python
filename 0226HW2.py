a9b39ba75d314e26010217f06d194433e1c1ca49
daspeichi <79582711+daspeichi@users.noreply.github.com>
2021-03-07 23:00:16 +0800

    Add files via upload

---
+++ 0226HW2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 22:20:55 2021
+
+@author: ElishaYi
+"""
+
+for i in range(10,1,-1):
+    for a in range (1,i):
+        print(a,end='')
+    print()
+
+'''
+i => [10,9,8,7,6,5,4,3,2]
+a => (1,10) => 123456789
+  => (1,9) => 12345678
+     .
+     .
+     .
+  =>(1,3) => 12
+  =>(1,2) => 1
+'''
+
